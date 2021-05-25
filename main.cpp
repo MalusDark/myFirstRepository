5e5db2f372b6cd69153e2f15f93fc96fb25b33d8
MalusDark <mcdabestkot@yandex.ru>
2021-05-25 08:49:31 +0300

    second commit

--- main.cpp
+++ main.cpp
@@ -2,8 +2,25 @@
 
 using namespace std;
 
+void input(int*, int*);
+
 int main()
 {
-          
-    return 0;
+	setlocale(LC_ALL, "rus");
+	
+	int num1, num2;
+	
+	input(&num1, &num2);
+	cout << "Числа: " << num1 << "; " << num2 << endl;
+
+	return 0;
+}
+
+void input(int *a, int *b)
+{
+	count << "Введите первое число ";
+	cin >> *a;
+	
+	cout << "Введите второе число ";
+	cin >> *b;
 }
